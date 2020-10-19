0486136a6cf04f0db5e59f5ae69f96cd38c950b4
Waqas Wahid <waqas_wahid001@outlook.com>
2020-10-19 19:14:35 +0500

    netlify identity
    netlify identity widget installation and setup in index.js

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { Container, Heading, Button, Flex } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget"
 
-export default props => (
-    <div>
-        <h1>Hello World </h1>
-    </div>
-);
+export default props => {
+    useEffect( () => {
+        netlifyIdentity.init({});
+    })
+    return (
+    <Container>
+        <Flex sx={{flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Todo App</Heading>
+        <Button sx={{marginTop: 2, color: 'black'}}
+            onClick= { () => {netlifyIdentity.open()}}
+            >Login</Button>
+        </Flex>
+    </Container>
+)}
